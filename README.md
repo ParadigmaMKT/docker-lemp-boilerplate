e30931528a36b73c0174158117395ffaee79fdaa
Greco Rubio <greco@paradigma.com.mx>
2015-12-11 00:58:59 -0600

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # docker-lemp-boilerplate
-Docker LEMP boilerplate
+Docker LEMP boilerplate for use with docker-compose
+
+Use this boilerplate for persistent database project.
+
+This is CentOS 7 linux based that uses 3 docker images:
+  - paradigma/c7-mariadb
+  - paradigma/c7-php-fpm
+  - paradigma/c7-nginx
+
+You can customize the virtual host NGINX config file.
+
+The project's root folder is src/public.
+
+You need to have docker preinstalled.
+
+You can customize DB credentials within db.env file.
+
+Run with:
+
+```bash
+docker-compose up -d
+```
+
+Once initialized, you can access database with any MySQL client.
+
+For running multiple instances, set the web container port number other than 8080
---
+++ src/public/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Test</title>
+    <meta content="IE=edge,chrome=1" name="X-UA-Compatible">
+    <meta content="no" name="apple-mobile-web-app-capable">
+    <meta content="black" name="apple-mobile-web-app-status-bar-style">
+    <meta content="telephone=no" name="format-detection">
+    <meta content="True" name="HandheldFriendly">
+    <meta content="320" name="MobileOptimized">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=0" name="viewport">
+    <meta content="Test" name="apple-mobile-web-app-title">
+    <link href="/favicon.ico" rel="shortcut icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" type="text/css">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style>
+        body {
+            padding-top: 50px;
+        }
+    </style>
+  </head>
+  <body>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Test</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="info.php">Info</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>¡Funciona!</h1>
+        <p class="lead">Ya puedes empezar tu desarrollo…</p>
+      </div>
+
+    </div>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  </body>
+</html>
